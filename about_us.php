5dbf9ed57e1378faa7e82dfe60deb0c16866bf8a
erwinmrpn <erwinkho2@gmail.com>
2025-12-07 23:46:16 +0700

    Menambahkan konten profil dosen di halaman about us

--- about_us.php
+++ about_us.php
@@ -135,16 +135,16 @@
                         </section>
                         <!-- Divider-->
                         <div class="pb-5"></div>
-                        <!-- Skills Section-->
+                        <!-- Dosen Section-->
                         <section>
-                            <!-- Skillset Card-->
+                            <!-- BOX Dosen-->
                             <div class="card shadow border-0 rounded-4 mb-5">
                                 <div class="card-body p-5">
-                                    <!-- Professional skills list-->
+                                    <!--list dosen -->
                                     <div class="mb-5">
                                         <div class="d-flex align-items-center mb-4">
-                                            <div class="feature bg-primary bg-gradient-primary-to-secondary text-white rounded-3 me-3"><i class="bi bi-tools"></i></div>
-                                            <h3 class="fw-bolder mb-0"><span class="text-gradient d-inline">Professional Skills</span></h3>
+                                            <div class="feature bg-primary bg-gradient-primary-to-secondary text-white rounded-3 me-3"><i class="bi bi-person-fill"></i></i></div>
+                                            <h3 class="fw-bolder mb-0"><span class="text-gradient d-inline">DOSEN</span></h3>
                                         </div>
                                         <div class="row row-cols-1 row-cols-md-3 mb-4">
                                             <div class="col mb-4 mb-md-0"><div class="d-flex align-items-center bg-light rounded-4 p-3 h-100">SEO/SEM Marketing</div></div>
@@ -157,11 +157,11 @@
                                             <div class="col"><div class="d-flex align-items-center bg-light rounded-4 p-3 h-100">User Interface Design</div></div>
                                         </div>
                                     </div>
-                                    <!-- Languages list-->
+                                    <!--list dosen lainnya-->
                                     <div class="mb-0">
                                         <div class="d-flex align-items-center mb-4">
-                                            <div class="feature bg-primary bg-gradient-primary-to-secondary text-white rounded-3 me-3"><i class="bi bi-code-slash"></i></div>
-                                            <h3 class="fw-bolder mb-0"><span class="text-gradient d-inline">Languages</span></h3>
+                                            <div class="feature bg-primary bg-gradient-primary-to-secondary text-white rounded-3 me-3"><i class="bi bi-person-fill"></i></div>
+                                            <h3 class="fw-bolder mb-0"><span class="text-gradient d-inline">DOSEN LAINNYA</span></h3>
                                         </div>
                                         <div class="row row-cols-1 row-cols-md-3 mb-4">
                                             <div class="col mb-4 mb-md-0"><div class="d-flex align-items-center bg-light rounded-4 p-3 h-100">HTML</div></div>
