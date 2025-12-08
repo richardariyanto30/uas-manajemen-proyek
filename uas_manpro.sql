b672d823790373e09edb334fba7d05644dd50871
richard <22n40005@student.unika.ac.id>
2025-12-08 17:11:04 +0700

    meereplace uas_manpro.sql

--- uas_manpro.sql
+++ uas_manpro.sql
@@ -1,98 +1,177 @@
-/*
-SQLyog Community v13.2.0 (64 bit)
-MySQL - 10.4.28-MariaDB : Database - uas_manpro
-*********************************************************************
-*/
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 08, 2025 at 11:10 AM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
 
-/*!40101 SET NAMES utf8 */;
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
-/*!40101 SET SQL_MODE=''*/;
 
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-CREATE DATABASE /*!32312 IF NOT EXISTS*/`uas_manpro` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
-USE `uas_manpro`;
+--
+-- Database: `uas_manpro`
+--
 
-/*Table structure for table `about_us` */
+-- --------------------------------------------------------
 
-DROP TABLE IF EXISTS `about_us`;
+--
+-- Table structure for table `about_us`
+--
 
 CREATE TABLE `about_us` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `id_kategori` int(11) DEFAULT NULL,
   `judul` varchar(255) DEFAULT NULL,
   `judul_panjang` varchar(255) DEFAULT NULL,
   `deskripsi` text DEFAULT NULL,
-  `gambar` longblob DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `gambar` longblob DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-/*Data for the table `about_us` */
+--
+-- Dumping data for table `about_us`
+--
 
-insert  into `about_us`(`id`,`id_kategori`,`judul`,`judul_panjang`,`deskripsi`,`gambar`) values 
-(2,1,'SIEGA','Sistem Informasi E-Commerce Game Technology AKSI','SIEGA merupakan sebuah singkatan dari beberapa konsentrasi yang ada di Program Studi Sistem Informasi Unika Soegijapranata Semarang yaitu, Sistem Informasi, E-Commerce, Game Technology, dan Akuntansi + Sistem Informasi.\r\n\r\nSIEGA berdiri sejak 11 November 2011 yang didirikan oleh Prof. Dr. Ridwan Sanjaya S.E., S.Kom., MS.IEC. Program Studi Sistem Informasi mendapatkan ijin penyelenggaraan serta akreditasinya melalui SK Menteri Pendidikan dan Kebudayaan RI No. 235/E/O/2013 pada tanggal 25 Juni 2013 dengan akreditasi pertama yaitu C.',NULL),
-(4,3,'AKREDITASI','Sistem Informasi E-Commerce Game Technology AKSI',NULL,'1765090053_sertifikat_akreditasi.png');
+INSERT INTO `about_us` (`id`, `id_kategori`, `judul`, `judul_panjang`, `deskripsi`, `gambar`) VALUES
+(2, 1, 'SIEGA', 'Sistem Informasi E-Commerce Game Technology AKSI', 'SIEGA merupakan sebuah singkatan dari beberapa konsentrasi yang ada di Program Studi Sistem Informasi Unika Soegijapranata Semarang yaitu, Sistem Informasi, E-Commerce, Game Technology, dan Akuntansi + Sistem Informasi.\r\n\r\nSIEGA berdiri sejak 11 November 2011 yang didirikan oleh Prof. Dr. Ridwan Sanjaya S.E., S.Kom., MS.IEC. Program Studi Sistem Informasi mendapatkan ijin penyelenggaraan serta akreditasinya melalui SK Menteri Pendidikan dan Kebudayaan RI No. 235/E/O/2013 pada tanggal 25 Juni 2013 dengan akreditasi pertama yaitu C.', NULL),
+(4, 1, 'AKREDITASI', 'Sistem Informasi E-Commerce Game Technology AKSI', NULL, 0x313736353039303035335f736572746966696b61745f616b72656469746173692e706e67),
+(5, 1, 'testing', 'testing testing', 'asdjasdasdasdasd', NULL),
+(6, 4, 'Brenda', 'kaprodi', NULL, 0x313736353138373535355f4c4c5842524e7e312e504e47),
+(7, 3, 'VISI', '', 'Menjadi komunitas akademik yang unggul dalam pendidikan, penelitian dan pengabdian dengan dilandasi nilai-nilai Kristiani: cinta kasih, keadilan dan kejujuran', NULL),
+(8, 3, 'MISI', '', '- Menyelenggarakan pendidikan yang berkualitas secara akademik dengan didukung pengembangan kepribadian yang utuh dan potensi kepemimpinan.\r\n- Melakukan penelitian untuk pengembangan ilmu dan teknologi demi meningkatkan kesejahteraan manusia.\r\n- Melakukan pengabdian kepada masyarakat sebagai penerapan ilmu dan teknologi yang telah dikembangkan dalam penelitian dan kesejahteraan manusia.\r\n- Memberikan perhatian dan mencari pemecahan terhadap berbagai masalah sosial budaya masyarakat melalui komunitas akademik.\r\n- Mengembangkan jaringan Kerjasama dengan berbagai institusi pendidikan, penelitian dan pengabdian lokal, nasional dan internasional untuk meningkatkan kualitas pendidikan dan penelitian.\r\n- Memperbaiki dan mengembangkan universitas secara terus menerus, sehingga dapat mendukung segala upaya untuk mencapai keunggulan.', NULL);
 
-/*Table structure for table `contact_messages` */
+-- --------------------------------------------------------
 
-DROP TABLE IF EXISTS `contact_messages`;
+--
+-- Table structure for table `contact_messages`
+--
 
 CREATE TABLE `contact_messages` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `fullname` varchar(150) NOT NULL,
   `email` varchar(150) NOT NULL,
   `phone` varchar(20) NOT NULL,
   `message` text NOT NULL,
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-/*Data for the table `contact_messages` */
+--
+-- Dumping data for table `contact_messages`
+--
 
-insert  into `contact_messages`(`id`,`fullname`,`email`,`phone`,`message`,`created_at`) values 
-(1,'evan','evan@gmail.com','082138539834','selamat pagi testing','2025-12-06 19:55:29'),
-(2,'Friend Ship','evansantoso138@gmail.com','+62 821 3853 9834','testing','2025-12-06 20:11:15'),
-(3,'Evan SSANTOSO','evansantoso138@gmail.com','+62 821 3853 9834','testing dulu','2025-12-06 20:12:29');
+INSERT INTO `contact_messages` (`id`, `fullname`, `email`, `phone`, `message`, `created_at`) VALUES
+(1, 'evan', 'evan@gmail.com', '082138539834', 'selamat pagi testing', '2025-12-06 12:55:29'),
+(2, 'Friend Ship', 'evansantoso138@gmail.com', '+62 821 3853 9834', 'testing', '2025-12-06 13:11:15'),
+(3, 'Evan SSANTOSO', 'evansantoso138@gmail.com', '+62 821 3853 9834', 'testing dulu', '2025-12-06 13:12:29'),
+(4, 'Richard Ariyanto Saputra', 'richardariyanto30@gmail.com', '081234567890', 'kerja sama yuk', '2025-12-08 10:01:44');
 
-/*Table structure for table `tabel_kategori_about_us` */
+-- --------------------------------------------------------
 
-DROP TABLE IF EXISTS `tabel_kategori_about_us`;
+--
+-- Table structure for table `tabel_kategori_about_us`
+--
 
 CREATE TABLE `tabel_kategori_about_us` (
-  `id_kategori` int(11) NOT NULL AUTO_INCREMENT,
+  `id_kategori` int(11) NOT NULL,
   `kategori` varchar(255) DEFAULT NULL,
-  `time_stamp` timestamp NOT NULL DEFAULT current_timestamp(),
-  PRIMARY KEY (`id_kategori`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `time_stamp` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-/*Data for the table `tabel_kategori_about_us` */
+--
+-- Dumping data for table `tabel_kategori_about_us`
+--
 
-insert  into `tabel_kategori_about_us`(`id_kategori`,`kategori`,`time_stamp`) values 
-(1,'sejarah SIEGA','2025-12-07 13:02:29'),
-(3,'Visi & Misi SIEGA','2025-12-07 13:18:02');
+INSERT INTO `tabel_kategori_about_us` (`id_kategori`, `kategori`, `time_stamp`) VALUES
+(1, 'sejarah SIEGA', '2025-12-07 06:02:29'),
+(3, 'Visi & Misi SIEGA', '2025-12-07 06:18:02'),
+(4, 'dosen', '2025-12-08 09:50:37');
 
-/*Table structure for table `user` */
+-- --------------------------------------------------------
 
-DROP TABLE IF EXISTS `user`;
+--
+-- Table structure for table `user`
+--
 
 CREATE TABLE `user` (
-  `id_user` int(10) NOT NULL AUTO_INCREMENT,
+  `id_user` int(10) NOT NULL,
   `username` varchar(100) DEFAULT NULL,
   `password` varchar(255) DEFAULT NULL,
   `role` enum('admin') DEFAULT NULL,
-  `timestamp` timestamp NOT NULL DEFAULT current_timestamp(),
-  PRIMARY KEY (`id_user`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `timestamp` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-/*Data for the table `user` */
+--
+-- Dumping data for table `user`
+--
 
-insert  into `user`(`id_user`,`username`,`password`,`role`,`timestamp`) values 
-(1,'admin','21232f297a57a5a743894a0e4a801fc3','admin','2025-12-06 19:39:26');
+INSERT INTO `user` (`id_user`, `username`, `password`, `role`, `timestamp`) VALUES
+(1, 'admin', '21232f297a57a5a743894a0e4a801fc3', 'admin', '2025-12-06 12:39:26');
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `about_us`
+--
+ALTER TABLE `about_us`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `contact_messages`
+--
+ALTER TABLE `contact_messages`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tabel_kategori_about_us`
+--
+ALTER TABLE `tabel_kategori_about_us`
+  ADD PRIMARY KEY (`id_kategori`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id_user`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `about_us`
+--
+ALTER TABLE `about_us`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `contact_messages`
+--
+ALTER TABLE `contact_messages`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `tabel_kategori_about_us`
+--
+ALTER TABLE `tabel_kategori_about_us`
+  MODIFY `id_kategori` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id_user` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
